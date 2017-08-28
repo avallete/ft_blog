8c89b4acf90cb0ac491b7f79b16dbad7fa444349
Andrew Valleteau <avallete@student.42.fr>
2017-08-28 19:45:57 +0200

    [FIX] Add some documentation for dev with docker

--- web/app_dev.php
+++ web/app_dev.php
@@ -10,6 +10,8 @@ use Symfony\Component\HttpFoundation\Request;
 
 // This check prevents access to debug front controllers that are deployed by accident to production servers.
 // Feel free to remove this, extend it, or make something more sophisticated.
+// Here we add '::ffff:172.18.0.1' into the array to allow Docker usage.
+// You can see yours by runing `docker inspect  $(docker ps -q) | grep Gateway`
 if (isset($_SERVER['HTTP_CLIENT_IP'])
     || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
     || !(in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', '::1', '::ffff', '::ffff:172.18.0.1'), true) || PHP_SAPI === 'cli-server')
